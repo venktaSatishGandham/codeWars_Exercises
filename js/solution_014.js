3317d6cedc44b993d7b17aef7147f906c3a3a935
venktaSatishGandham <satishgandham728@gmail.com>
2022-06-05 22:41:48 +0530

    solutions of 13, 14 & 15 are added

---
+++ js/solution_014.js
@@ -0,0 +1,9 @@
+
+/* Code as fast as you can! You need to double the integer and return it.*/
+
+
+
+function doubleInteger(i) {
+    return 2 * i;
+}
+//console.log(doubleInteger(54));
---
+++ js/solution_015.js
@@ -0,0 +1,4 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+square = n => n * n;
+// console.log(square(8));

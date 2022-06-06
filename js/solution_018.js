032b6f8516e2405063f069e2ad51779d538fc8a9
venktaSatishGandham <satishgandham728@gmail.com>
2022-06-06 22:09:04 +0530

    solutions of 16, 17, 18, 19 & 20 are added

---
+++ js/solution_018.js
@@ -0,0 +1,12 @@
+/* Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]*/
+
+
+function stringToArray(string) {
+    let d = string.split(" ");
+    return d;
+}

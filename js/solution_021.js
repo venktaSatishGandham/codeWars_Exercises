a6cbe51dc023ff37fc57af920e1b850c8e0e313d
venktaSatishGandham <satishgandham728@gmail.com>
2022-06-07 23:46:37 +0530

    solution of 21 added

---
+++ js/solution_021.js
@@ -0,0 +1,12 @@
+/* Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+348597 => [7,9,5,8,4,3]
+0 => [0] */
+
+
+function digitize(n) {
+
+    var d = String(n).split('').reverse();
+    return d.map(Number);
+}
